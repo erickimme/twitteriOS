dc82b129f8bdd9c452b85e9abd9010af737de480
JaeYeon <kim802@purdue.edu>
2018-04-08 00:32:00 -0400

    model setup

--- twitter_alamofire_demo/User.swift
+++ twitter_alamofire_demo/User.swift
@@ -10,10 +10,25 @@ import Foundation
 
 class User {
     
-    var name: String
+    var name: String?
+    var screenName: String?
+    var profile: String?
+    var profileImage: URL?
+    var numFollowers: Int?
+    var numFavorites: Int?
+    var totalTweets: Int?
+    var imageUrl: String
+    var description: String?
     
     init(dictionary: [String: Any]) {
         name = dictionary["name"] as! String
-
+        screenName = dictionary["screen_name"] as? String
+        profile = dictionary["profile_image_url_https"] as? String
+        profileImage = URL(string: profile!)
+        numFollowers = dictionary["followers_count"] as? Int
+        numFavorites = dictionary["favorties_count"] as? Int
+        totalTweets = dictionary["statuses_count"] as? Int
+        imageUrl = dictionary["profile_image_url_https"] as! String
+        description = dictionary["description"] as? String
     }
 }
